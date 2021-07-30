be4f7654910ad8db9b3b27f2065c716f0c0a0502
kondalu4b9 <kondalu4b9@gmail.com>
2021-07-30 16:41:47 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -4,10 +4,10 @@ provider "aws" {
    #secret_key  =  "${ secrets.AWS_SECRET_ACCESS_KEY }"
 }
 
-resource "aws_instance" "my-first-server" {
-   ami           = "ami-0c2b8ca1dad447f8a"
-   instance_type = "t2.micro"
-   tags = {
-      Name = "Ubuntu"
-   }
-}
+#resource "aws_instance" "my-first-server" {
+#   ami           = "ami-0c2b8ca1dad447f8a"
+#   instance_type = "t2.micro"
+#   tags = {
+#      Name = "Ubuntu"
+#   }
+#}
